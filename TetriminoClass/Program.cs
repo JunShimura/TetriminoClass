767bb8523d1ad153a42a4a0d9bce3f4fea1b2890
Jun Shimura <junshimura@gmail.com>
2020-08-17 09:29:30 +0900

    プロジェクト ファイルを追加します。

---
+++ TetriminoClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace TetriminoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] mino = { 'I', 'O', 'S', 'Z', 'J', 'L', 'T' };
+            Tetrimino tetrimino = new Tetrimino(mino);
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(tetrimino.NextMino());
+            }
+        }
+    }
+}
---
+++ TetriminoClass/Tetrimino.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetriminoClass
+{
+    public class Tetrimino
+    {
+        private List<char> minos;
+        public Tetrimino(char[] minos)
+        {
+            this.minos = new List<char>(minos);
+        }
+        public char NextMino()
+        {
+            //ここの処理を考える
+            char next = minos[0];
+
+            return next;
+        }
+    }
+}
